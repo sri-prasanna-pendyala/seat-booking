084ea2c982a4b02fd1f55a47e016ec1bddd55238
sri prasanna <sriprasanna110@gmail.com>
2026-03-14 10:51:09 +0530

    Final changes made

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const pool = require('./db');
 const routes = require('./routes/index');
 
 const app = express();
-const PORT = process.env.API_PORT || 8080;
+const PORT = process.env.API_PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
